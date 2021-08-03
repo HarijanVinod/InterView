df9774d8a841e464d6310f7218d87c612d0d9a59
Lokesh Pandey <pandeylokesh44@gmail.com>
2021-08-03 13:50:17 +0530

    Unit Test

---
+++ TestInsuranceClaim/Controllers/LoadLossTypeDetailsControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using InterviewDemo.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TestInsuranceClaim.Controllers.Tests
+{
+    [TestClass]
+    public class LoadLossTypeDetailsControllerTest
+    {
+        [TestMethod]
+        public void GetLossTypeDataTest()
+        {
+            LoadLossTypeDetailsController loadLossTypeDetailsController = new LoadLossTypeDetailsController();
+            ViewResult result = loadLossTypeDetailsController.GetLossTypeData() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TestInsuranceClaim/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestInsuranceClaim")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestInsuranceClaim")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("658039fb-616b-4fcd-a508-5b6700e0b713")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
